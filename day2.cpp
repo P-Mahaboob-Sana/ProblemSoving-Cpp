6db0d22719b428ea9338828ce81d1529b765b37d
P-Mahaboob-Sana <129360011+P-Mahaboob-Sana@users.noreply.github.com>
2023-06-13 19:48:31 +0530

    Add files via upload

---
+++ day2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int berthNo;
+	cout<<"Enter your Berth No:"<<endl;
+	cin>>berthNo;
+	
+	switch(berthNo){
+		case 1:
+			cout<<"Your pair berth is "<<berthNo+3<<"LB"<<endl;
+			break;
+	
+	case 2:
+			cout<<"Your pair berth is "<<berthNo+3<<"MB"<<endl;
+			break;
+			
+			case 3:
+			cout<<"Your pair berth is "<<berthNo+3<<"UB"<<endl;
+			break;
+			
+			case 4:
+			cout<<"Your pair berth is "<<berthNo-3<<"LB"<<endl;
+			break;
+			
+			case 5:
+			cout<<"Your pair berth is "<<berthNo-3<<"MB"<<endl;
+			break;
+			
+		case 6:
+			cout<<"Your pair berth is "<<berthNo-3<<"UB"<<endl;
+			break;
+			case 7:
+			cout<<"Your pair berth is "<<berthNo+1<<"SU"<<endl;
+			break;
+			case 8:
+			cout<<"Your pair berth is "<<berthNo-1<<"SL"<<endl;
+			break;
+			
+			default:
+				cout<<"invalid berth no"<<endl;
+		}
+		return 0;
+}
